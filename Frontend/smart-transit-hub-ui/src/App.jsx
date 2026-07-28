b38692bcf95893ca5506b2255cb5d56e62b456db
Kaustav <kaustavsinha14@gmail.com>
2026-07-28 13:04:05 +0530

    Frontend folder added

---
+++ Frontend/smart-transit-hub-ui/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Smart Transit Hub</h1>
+      <h2>Frontend Successfully Initialized 🚍</h2>
+    </div>
+  );
+}
+
+export default App;
